878bd8c23933d628bf750378bbe527b841901c3d
Lach <iam@lach.pw>
2025-09-28 19:28:10 +0200

    build: update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Fleet terraform integration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
