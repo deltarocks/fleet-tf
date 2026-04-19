903fb2e976e35fba4ade03dc6a454dca1d2c5ce9
Yaroslav Bolyukin <iam@lach.pw>
2026-04-19 21:29:58 +0200

    build: update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Fleet terraform integration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
